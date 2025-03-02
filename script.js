efb55f7244be252dc6df054c1ceae4001d8ffd8a
Tim Kaiser <you@example.com>
2025-03-02 17:14:05 +0100

    ha

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let pgnText;
 document.getElementById("analyze").addEventListener("click", function(){
     let pgnInput = document.getElementById("pgn_input").value;
 
-    fetch("http://127.0.0.1:5000/analyze",{
+    fetch("https://ssehc-backend.onrender.com/analyze",{
         method:"POST",
         headers: {
             "Content-Type": "application/json"
