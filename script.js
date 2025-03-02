7a208d32975d18b091f2cdd58a60d99ca4707165
Tim Kaiser <you@example.com>
2025-03-02 15:51:13 +0100

    rrrr

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+let pgnText;
 
 document.getElementById("analyze").addEventListener("click", function(){
-    let pgnText = document.getElementById("pgn_input").value;
+    let pgnInput = document.getElementById("pgn_input").value;
 
     fetch("http://127.0.0.1:5000/analyze",{
         method:"POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ pgn: pgnText})
+        body: JSON.stringify({ pgn: pgnInput})
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById("output").textContent = data.message + "\n" + data.pgn;
+        pgnText = data.pgn;
+        document.getElementById("output").textContent = data.pgn;
         console.log("Server response", data);
+        console.log(pgnText);
     })
     .catch(error => console.error("Error:", error));
+
 
 });
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import chess.pgn
 
 
-app = Flask(__name__)
-CORS(app)
+server1 = Flask(__name__)
+CORS(server1)
 
-@app.route('/analyze', methods=['POST'])
+@server1.route('/analyze', methods=['POST'])
 def analyze_pgn():
     data = request.json  # Die JSON-Daten von JavaScript holen
     pgn_text = data.get("pgn", "")
@@ -16,4 +17,9 @@ def analyze_pgn():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    server1.run(debug=True)
+
+
+def process_pgn(pgn_Text):
+    print("Pgn: ", pgn_Text)
+
