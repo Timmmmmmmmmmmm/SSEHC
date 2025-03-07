4b67437d6ee211710f68f7487bc5156c9542798e
Tim Kaiser <you@example.com>
2025-03-07 15:29:47 +0100

    Stockfish over linux-server

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Downloading Stockfish..."
+wget -O stockfish https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-linux-x86-64-avx2
+chmod +x stockfish
+echo "Stockfish is ready!"
+python server.py
+gunicorn server:SSEHC  
